6151c18ad7194c9c87bda128810ce3ce38837464
Liang-hsiao <marsgt@qq.com>
2019-03-21 21:36:15 +0800

    feature: 增加 Service Worker 缓存的版本检查和替换

--- js/sw.js
+++ js/sw.js
@@ -22,17 +22,35 @@ const fileList = [
     '/img/10.jpg',
 ];
 
+// 安装阶段开启缓存
 self.addEventListener('install', (ev) => {
-    console.info("Install SW");
+    console.info("Install:SW");
     ev.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => { cache.addAll(fileList) })
     )
 });
 
+// 枚举原有缓存并检查版本标记，如果比现有版本旧那么就删掉旧版
+self.addEventListener('activate', (ev) => {
+    console.info('Activate:SW');
+    ev.waitUntil(
+        caches.keys()
+            .then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.info('Removing[', key, ']:SW');
+                        return caches.delete(key);
+                    }
+                }));
+            })
+    );
+});
+
+// 拦截fetch处理返回的Request
 self.addEventListener('fetch', (ev) => {
     ev.respondWith(
-        caches.match(event.request)
+        caches.match(ev.request)
             .then((res) => { res || fetch(ev.request) })
     );
 });
